f17dd22f0d35883f04f4470a3659e39d21a5c874
MarijeWouterse <102292506+MarijevandePoel@users.noreply.github.com>
2023-03-14 10:35:00 +0100

    Update AboutUs.js

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -4,8 +4,7 @@ function AboutUs() {
 			<h1>About Us</h1>
 			<p>
 				This exercise is about learning React en using Redux. For my state of
-				the songs I used the Store from Redux. As actions I defined the filter,
-				sort and delete functions to change the state.
+				the songs I used the Store from Redux. As action I created a delete function to change the state.
 			</p>
 			<p>this is the second paragraph with some more content.</p>
 		</div>
